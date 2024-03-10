6d21fe881b8983cd9d713ee10c2c1b12d9727d36
laroyprado <laroyprado@gmail.com>
2024-03-09 23:20:03 -0300

    feat: inicia backend

---
+++ Oracle-Partner-Tracker/src/main/java/Oracle/Partner/Tracker/OraclePartnerTrackerApplication.java
@@ -0,0 +1,13 @@
+package Oracle.Partner.Tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OraclePartnerTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OraclePartnerTrackerApplication.class, args);
+	}
+
+}
---
+++ Oracle-Partner-Tracker/src/test/java/Oracle/Partner/Tracker/OraclePartnerTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package Oracle.Partner.Tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OraclePartnerTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
