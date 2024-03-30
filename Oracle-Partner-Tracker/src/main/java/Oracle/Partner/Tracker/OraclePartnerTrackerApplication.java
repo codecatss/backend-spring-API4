57d3cfe2389c7a9e6cec472376dc7e43f5c391fe
laroyprado <laroyprado@gmail.com>
2024-03-30 12:40:32 -0300

    feat: atualiza mensagem de inicio do projeto

--- Oracle-Partner-Tracker/src/main/java/Oracle/Partner/Tracker/OraclePartnerTrackerApplication.java
+++ Oracle-Partner-Tracker/src/main/java/Oracle/Partner/Tracker/OraclePartnerTrackerApplication.java
@@ -8,6 +8,12 @@ public class OraclePartnerTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OraclePartnerTrackerApplication.class, args);
+
+		System.out.println("Server is running");
+
+		System.out.println("  /\\_/\\");
+		System.out.println(" ( o.o )");
+		System.out.println("  > ^ <");
 	}
 
 }
