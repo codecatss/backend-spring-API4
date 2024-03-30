09aef1393e58b4098ea348e861d017ca4fc6a8c4
laroyprado <laroyprado@gmail.com>
2024-03-30 12:41:01 -0300

    feat: Cria repository de company

---
+++ Oracle-Partner-Tracker/src/main/java/Oracle/Partner/Tracker/repositories/CompanyRepository.java
@@ -0,0 +1,7 @@
+package Oracle.Partner.Tracker.repositories;
+
+import Oracle.Partner.Tracker.entities.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanyRepository extends JpaRepository <Company,String>{
+}
