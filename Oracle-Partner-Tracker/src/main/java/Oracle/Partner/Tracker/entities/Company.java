a53afd6776291afb634f50964ab64442c04abad6
laroyprado <laroyprado@gmail.com>
2024-03-30 12:40:42 -0300

    feat: cria entidade company

---
+++ Oracle-Partner-Tracker/src/main/java/Oracle/Partner/Tracker/entities/Company.java
@@ -0,0 +1,53 @@
+package Oracle.Partner.Tracker.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Table(name = "company")
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "opn_status" )
+    private Boolean opnStatus;
+
+    @Column(name = "cnpj",unique = true, nullable = false,length = 150)
+    private String cnpj;
+
+    @Column(name="country", nullable = false,length = 20)
+    private String country;
+
+    @Column(name = "state",nullable = false,length = 50)
+    private String state;
+
+    @Column(name="city", nullable = false,length = 100)
+    private String city;
+
+    @Column(name = "address", nullable = false,length = 200)
+    private String address;
+
+    @Column(name = "cep", nullable = false,length = 10)
+    private String cep;
+
+    @Column(name="create_on")
+    private LocalDateTime createOn;
+
+
+}
