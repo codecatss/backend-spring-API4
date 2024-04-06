09c47d6808addc831ae81342fe93a36e256ca57c
Lukas <lkfernando12345@gmail.com>
2024-04-06 15:12:56 -0300

    feat: ajustando a expertise para que use um enum no status

--- Oracle-Partner-Tracker/src/main/java/Oracle/Partner/Tracker/entities/Expertise.java
+++ Oracle-Partner-Tracker/src/main/java/Oracle/Partner/Tracker/entities/Expertise.java
@@ -17,7 +17,7 @@ import java.time.LocalDateTime;
 @Table(name = "service_expertise")
 public class Expertise {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
 
     @Column(name = "name", nullable = true, length = 100)
@@ -26,21 +26,16 @@ public class Expertise {
     @Column(name = "description", nullable = true, length = 250)
     private String description;
 
-    @Column(name = "min_score", nullable = true)
-    private Integer minScore;
-
-    @Column(name = "max_score", nullable = true)
-    private Integer maxScore;
-
     @Column(name = "life_time_month", nullable = true)
     private Integer lifeTimeMonth;
 
+    @Column(name = "status")
+    private Status status;
+
     public Expertise(ExpertiseDTO expertiseDTO) {
-//        this.id = expertiseDTO.getId();
         this.name = expertiseDTO.getName();
         this.description = expertiseDTO.getDescription();
-        this.minScore = expertiseDTO.getMinScore();
-        this.maxScore = expertiseDTO.getMaxScore();
         this.lifeTimeMonth = expertiseDTO.getLifeTimeMonth();
+        this.status = expertiseDTO.getStatus();
     }
 }
