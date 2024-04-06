f38150ee5381c4b26768f275482c804c98b9c210
Larissa Fernanda <laliffr@gmail.com>
2024-04-05 21:57:25 -0300

    [CATS-26] feat(OpnTrack): Adiciona Getters e Setters

--- Oracle-Partner-Tracker/src/main/java/Oracle/Partner/Tracker/entities/OpnTrack.java
+++ Oracle-Partner-Tracker/src/main/java/Oracle/Partner/Tracker/entities/OpnTrack.java
@@ -2,9 +2,11 @@ package Oracle.Partner.Tracker.entities;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "opn_track")
