03780458e0f590bdacca43ed0d03c5573ace01f0
Lukas <lkfernando12345@gmail.com>
2024-04-06 14:48:00 -0300

    feat: arrumando o ddl. Mudando o id de varchar para int, adicionando status em todas as entity, e adicionando check e tirando os boolean

--- Oracle-Partner-Tracker/ddl.sql
+++ Oracle-Partner-Tracker/ddl.sql
@@ -13,21 +13,22 @@ use oracle_partner_network;
 
 -- Create table section
 create table user (
-    id varchar(50) not null,
+    id bigint unsigned not null auto_increment,
     email varchar(50) unique,
     password varchar(100) not null,
     name varchar(50),
     role varchar(50),
     membership_type varchar(10),
-    user_status varchar(5),
+    status varchar(10),
     create_on timestamp default current_timestamp,
-    constraint ck_role check (UPPER(role) IN ('ADM', 'USER')),
+    constraint ck_role check (role IN ('ADM', 'USER')),
+    constraint ck_user_status check (status IN ('ACTIVE', 'INACTIVE')),
     constraint ck_membership_type check (UPPER(membership_type) IN ('PRINCIPAL', 'WORLDWIDE')),
     primary key (id)
 );
 
 create table company (
-    id varchar(50) not null,
+    id bigint unsigned not null auto_increment,
     name varchar(50),
     slogan varchar(200),
     credit_hold boolean,
@@ -37,89 +38,95 @@ create table company (
     city varchar(100),
     address varchar(200),
     zip_code varchar(10),
-    opn_status boolean,
-    company_status boolean,
+    opn_status varchar(15),
+    status varchar(10),
+    constraint ck_company_status check (status IN ('ACTIVE', 'INACTIVE')),
+    constraint ck_opn_status check (opn_status IN ('MEMBER')),
     create_on timestamp default current_timestamp,
     primary key (id)
 );
 
 create table workload (
-    id varchar(50) not null,
+    id bigint unsigned not null auto_increment,
     name varchar(100) unique,
     description varchar(250),
+    status varchar(10),
+    constraint ck_workload_status check (status IN ('ACTIVE', 'INACTIVE')),
     primary key (id)
 );
 
 create table service_expertise (
-    id varchar(50) not null,
-    name varchar(100),
+    id bigint unsigned not null auto_increment,
+    name varchar(100) unique,
     description varchar(250),
-    min_score int,
-    max_score int,
     life_time_month int,
+    status varchar(10),
+    constraint ck_expertise_status check (status IN ('ACTIVE', 'INACTIVE')),
     primary key (id)
 );
 
 create table opn_track (
-    id varchar(50) not null,
+    id bigint unsigned not null auto_increment,
     name varchar(20),
-    constraint ck_opn_track_name check (UPPER(name) IN ('CLOUD BUILD','CLOUD BUILD & SERVICE','CLOUD SELL','CLOUD SELL & SERVICE','CLOUD SERVICE','INDUSTRY HEALTHCARE','LICENSE AND HARDWARE','OPN MEMBER')),
+    status varchar(10),
+    constraint ck_opn_track_status check (status IN ('ACTIVE', 'INACTIVE')),
     primary key (id)
 );
 
 create table user_and_company (
-    id varchar(50) not null,
-    user_id varchar(50) not null unique,
-    company_id varchar(50) not null,
+    id bigint unsigned not null auto_increment,
+    user_id bigint unsigned not null unique,
+    company_id bigint unsigned not null,
     primary key (id),
     foreign key user_fk (user_id) references user (id) on delete restrict on update cascade,
     foreign key company_fk (company_id) references company (id) on delete restrict on update cascade
 );
 
 create table user_and_expertise (
-    id varchar(50) not null,
-    user_id varchar(50) not null,
-    expertise_id varchar(50) not null,
+    id bigint unsigned not null auto_increment,
+    user_id bigint unsigned not null,
+    expertise_id bigint unsigned not null,
     score numeric(2,2),
-    status boolean,
+    status varchar(10),
     create_on timestamp default current_timestamp,
     expiration_date timestamp,
+    constraint ck_user_and_expertise_status check (status IN ('ACTIVE', 'INACTIVE')),
     primary key (id),
     foreign key user_fk (user_id) references user (id) on delete restrict on update cascade,
     foreign key service_expertise_fk (expertise_id) references service_expertise (id) on delete restrict on update cascade
 );
 
 create table opn_track_and_expertise (
-    id varchar(50) not null,
-    opn_track_id varchar(50) not null,
-    expertise_id varchar(50) not null,
+    id bigint unsigned not null auto_increment,
+    opn_track_id bigint unsigned not null,
+    expertise_id bigint unsigned not null,
     primary key (id),
     foreign key opn_track_fk (opn_track_id) references opn_track (id) on delete restrict on update cascade,
     foreign key service_expertise_fk (expertise_id) references service_expertise (id) on delete restrict on update cascade
 );
 
 create table opn_track_and_company (
-    id varchar(50) not null,
-    opn_track_id varchar(50) not null,
-    company_id varchar(50) not null,
+    id bigint unsigned not null auto_increment,
+    opn_track_id bigint unsigned not null,
+    company_id bigint unsigned not null,
     primary key (id),
     foreign key opn_track_fk (opn_track_id) references opn_track (id) on delete restrict on update cascade,
     foreign key company_fk (company_id) references company (id) on delete restrict on update cascade
 );
 
 create table opn_track_and_workload (
-    id varchar(50) not null,
-    opn_track_id varchar(50) not null,
-    workload_id varchar(50) not null,
+    id bigint unsigned not null auto_increment,
+    opn_track_id bigint unsigned not null,
+    workload_id bigint unsigned not null,
     primary key (id),
     foreign key opn_track_fk (opn_track_id) references opn_track (id) on delete restrict on update cascade,
     foreign key workload_fk (workload_id) references workload (id) on delete restrict on update cascade
 );
 
 create table workload_and_expertise (
-    id varchar(50) not null,
-    workload_id varchar(50) not null,
-    expertise_id varchar(50) not null,
+    id bigint unsigned not null auto_increment,
+    workload_id bigint unsigned not null,
+    expertise_id bigint unsigned not null,
     primary key (id),
     foreign key workload_fk (workload_id) references workload (id) on delete restrict on update cascade,
     foreign key service_expertise_fk (expertise_id) references service_expertise (id) on delete restrict on update cascade
