737cfcdd6495993ca8b1847d30f1fedd0130120e
Larissa Fernanda <laliffr@gmail.com>
2024-04-06 23:30:08 -0300

    [CATS-26] feat(OpnTrackDTO): Adiciona atributo createdAt

--- Oracle-Partner-Tracker/src/main/java/Oracle/Partner/Tracker/dto/OpnTrackDTO.java
+++ Oracle-Partner-Tracker/src/main/java/Oracle/Partner/Tracker/dto/OpnTrackDTO.java
@@ -1,5 +1,7 @@
 package Oracle.Partner.Tracker.dto;
 
+import java.time.LocalDateTime;
+
 import Oracle.Partner.Tracker.entities.OpnTrack;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
@@ -20,10 +22,14 @@ public class OpnTrackDTO {
     @Schema(description = "Status da OPN Track", example = "true")
     private Boolean opnTrackStatus;
 
+    @Schema(description = "Data de criação da OpnTrack", example = "2022-01-01T12:00:00")
+    private LocalDateTime createdAt;
+
     public OpnTrackDTO(OpnTrack entity){
         this.id = entity.getId();
         this.name = entity.getName();
         this.opnTrackStatus = entity.getOpnTrackStatus();
+        this.createdAt = entity.getCreatedAt();
     }
     
 }
