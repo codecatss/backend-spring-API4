e530a1a6a7f7c6b281107c909575e3915bcdd794
Lukas <lkfernando12345@gmail.com>
2024-04-07 17:17:41 -0300

    feat: ajustes do ddl

--- Oracle-Partner-Tracker/ddl.sql
+++ Oracle-Partner-Tracker/ddl.sql
@@ -19,9 +19,10 @@ create table user (
     name varchar(50),
     role enum('ADM', 'USER'),
     membership_type enum('PRINCIPAL', 'WORLDWIDE'),
+    ingestion_operation enum('CSV', 'MANUAL'),
     status enum('ACTIVE', 'INACTIVE'),
-    created_on timestamp default current_timestamp,
-    updated_on timestamp default current_timestamp,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp,
     primary key (id)
 );
 
@@ -36,11 +37,11 @@ create table company (
     city varchar(100),
     address varchar(200),
     zip_code varchar(10),
-    opn_status enum('MEMBER'),
+    opn_status enum('MEMBER', 'EXPIRED'),
     ingestion_operation enum('CSV', 'MANUAL'),
     status enum('ACTIVE', 'INACTIVE'),
-    created_on timestamp default current_timestamp,
-    updated_on timestamp default current_timestamp,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp,
     primary key (id)
 );
 
@@ -48,9 +49,10 @@ create table workload (
     id bigint unsigned not null auto_increment,
     name varchar(100) unique,
     description varchar(250),
+    ingestion_operation enum('CSV', 'MANUAL'),
     status enum('ACTIVE', 'INACTIVE'),
-    created_on timestamp default current_timestamp,
-    updated_on timestamp default current_timestamp,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp,
     primary key (id)
 );
 
@@ -59,18 +61,20 @@ create table service_expertise (
     name varchar(100) unique,
     description varchar(250),
     life_time_month int,
+    ingestion_operation enum('CSV', 'MANUAL'),
     status enum('ACTIVE', 'INACTIVE'),
-    created_on timestamp default current_timestamp,
-    updated_on timestamp default current_timestamp,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp,
     primary key (id)
 );
 
 create table opn_track (
     id bigint unsigned not null auto_increment,
     name varchar(20),
+    ingestion_operation enum('CSV', 'MANUAL'),
     status enum('ACTIVE', 'INACTIVE'),
-    created_on timestamp default current_timestamp,
-    updated_on timestamp default current_timestamp,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp,
     primary key (id)
 );
 
@@ -97,8 +101,8 @@ create table company_and_workload (
     company_id bigint unsigned not null,
     workload_id bigint unsigned not null,
     score numeric(3,2),
-    status enum('PASSED', 'FAILED'),
-    created_on timestamp default current_timestamp,
+    status enum('PASSED', 'IN PROGRESS', 'EXPIRED'),
+    created_at timestamp default current_timestamp,
     expiration_date timestamp,
     primary key (id),
     foreign key company_fk (company_id) references company (id) on delete restrict on update cascade,
@@ -122,35 +126,4 @@ create table workload_and_expertise (
     foreign key workload_fk (workload_id) references workload (id) on delete restrict on update cascade,
     foreign key service_expertise_fk (expertise_id) references service_expertise (id) on delete restrict on update cascade
 );
-
-
-
-
-/*
-
-NÃO VAI TER MAIS ESSAS DE BAIXO, APAGAR!!!!!
-
-create table user_and_expertise (
-    id bigint unsigned not null auto_increment,
-    user_id bigint unsigned not null,
-    expertise_id bigint unsigned not null,
-    score numeric(2,2),
-    status enum('ACTIVE', 'INACTIVE'),
-    created_on timestamp default current_timestamp,
-    expiration_date timestamp,
-    primary key (id),
-    foreign key user_fk (user_id) references user (id) on delete restrict on update cascade,
-    foreign key service_expertise_fk (expertise_id) references service_expertise (id) on delete restrict on update cascade
-);
-
-create table opn_track_and_workload (
-    id bigint unsigned not null auto_increment,
-    opn_track_id bigint unsigned not null,
-    workload_id bigint unsigned not null,
-    primary key (id),
-    foreign key opn_track_fk (opn_track_id) references opn_track (id) on delete restrict on update cascade,
-    foreign key workload_fk (workload_id) references workload (id) on delete restrict on update cascade
-);
-
-*/
 
