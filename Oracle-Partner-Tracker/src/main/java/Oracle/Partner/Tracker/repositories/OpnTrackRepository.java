26cf995326ed4d35e3fa37089da478c8f6d4298b
Larissa Fernanda <laliffr@gmail.com>
2024-04-08 19:52:39 -0300

    feat(OpnTrackRepository): Adiciona método findById

--- Oracle-Partner-Tracker/src/main/java/Oracle/Partner/Tracker/repositories/OpnTrackRepository.java
+++ Oracle-Partner-Tracker/src/main/java/Oracle/Partner/Tracker/repositories/OpnTrackRepository.java
@@ -2,8 +2,12 @@ package Oracle.Partner.Tracker.repositories;
 
 import Oracle.Partner.Tracker.entities.OpnTrack;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OpnTrackRepository extends JpaRepository <OpnTrack,Long>{
+    Optional<OpnTrack> findById(Long id);
+
     OpnTrack findByName(String name);   
 } 
