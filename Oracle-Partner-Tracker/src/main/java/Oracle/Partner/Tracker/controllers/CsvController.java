19c783f5a20468bb2b1f4ccb7e349232d800a4c3
Danko Caboski <caboskidanko@gmail.com>
2024-04-10 19:27:54 -0300

    Feat: Global CORS config

--- Oracle-Partner-Tracker/src/main/java/Oracle/Partner/Tracker/controllers/CsvController.java
+++ Oracle-Partner-Tracker/src/main/java/Oracle/Partner/Tracker/controllers/CsvController.java
@@ -14,7 +14,6 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-@CrossOrigin
 public class CsvController {
 
     @Autowired
---
+++ Oracle-Partner-Tracker/src/main/java/Oracle/Partner/Tracker/infra/security/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+package Oracle.Partner.Tracker.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.filter.CorsFilter;
+
+public class WebSecurityConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(false);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+            .requestMatchers(new AntPathRequestMatcher("/**"));
+    }
+}
