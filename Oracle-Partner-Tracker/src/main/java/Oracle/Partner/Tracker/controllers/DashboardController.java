6a7239cfa466343ad3bb8cf5cf5d00cf30fab611
laroyprado <laroyprado@gmail.com>
2024-04-28 18:04:27 -0300

    [CATS-45] - Cria enpoint "/companyexpertiseusercountservice"

--- Oracle-Partner-Tracker/src/main/java/Oracle/Partner/Tracker/controllers/DashboardController.java
+++ Oracle-Partner-Tracker/src/main/java/Oracle/Partner/Tracker/controllers/DashboardController.java
@@ -1,8 +1,11 @@
 package Oracle.Partner.Tracker.controllers;
 
+import Oracle.Partner.Tracker.entities.CompanyExpertiseUserCount;
 import Oracle.Partner.Tracker.dto.DashboardDTO;
 import Oracle.Partner.Tracker.dto.TrackPerCompany;
 import Oracle.Partner.Tracker.dto.StatePerCompany;
+import Oracle.Partner.Tracker.repositories.CompanyRepository;
+import Oracle.Partner.Tracker.services.CompanyExpertiseUserCountService;
 import Oracle.Partner.Tracker.services.DashboardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -23,41 +26,51 @@ public class DashboardController {
     private DashboardService dashboardService;
 
     @GetMapping
-    public ResponseEntity<DashboardDTO> getAllKPI(){
+    public ResponseEntity<DashboardDTO> getAllKPI() {
 
         DashboardDTO data = dashboardService.getAll();
-        if(data == null){
+        if (data == null) {
             return ResponseEntity.notFound().build();
         }
-        return  ResponseEntity.ok(data);
+        return ResponseEntity.ok(data);
     }
 
-    @GetMapping(value="/track-per-company")
-    public ResponseEntity<List<TrackPerCompany>> getTrackPerCompany(){
+    @GetMapping(value = "/track-per-company")
+    public ResponseEntity<List<TrackPerCompany>> getTrackPerCompany() {
 
         List<TrackPerCompany> data = dashboardService.getTrackPerCompany();
-        if(data == null){
+        if (data == null) {
             return ResponseEntity.notFound().build();
         }
-        return  ResponseEntity.ok(data);
+        return ResponseEntity.ok(data);
     }
 
-    @GetMapping(value="/state-per-company")
-    public ResponseEntity<List<StatePerCompany>> getStatePerCompany(){
+    @GetMapping(value = "/state-per-company")
+    public ResponseEntity<List<StatePerCompany>> getStatePerCompany() {
         List<StatePerCompany> data = dashboardService.getStatePerCompany();
-        if(data == null){
+        if (data == null) {
             return ResponseEntity.notFound().build();
         }
-        return  ResponseEntity.ok(data);
+        return ResponseEntity.ok(data);
     }
 
     @GetMapping(path = "/opntrack/visualization")
-    public Map<Integer, Map<String, String>> getOpnTrackUsageCount(){
+    public Map<Integer, Map<String, String>> getOpnTrackUsageCount() {
         return dashboardService.getOpnTrackUsageCount();
     }
 
     @GetMapping(path = "/expertise/visualization")
-    public Map<Integer, Map<String, String>> getExpertiseUsageCount(){
+    public Map<Integer, Map<String, String>> getExpertiseUsageCount() {
         return dashboardService.getExpertiseUsageCount();
     }
+
+
+    @Autowired
+    CompanyExpertiseUserCountService companyExpertiseUserCountService;
+
+    @GetMapping(path = "/companyexpertiseusercountservice")
+    public List<CompanyExpertiseUserCount> getCompanyExpertiseUserCount() {
+        return companyExpertiseUserCountService.findAllCompanies();
+    }
+
 }
