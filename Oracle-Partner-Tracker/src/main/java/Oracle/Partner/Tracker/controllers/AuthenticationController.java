fa68270f18b016a423ec5df579a2414c8ead7ed2
dankocaboski <caboskidanko@gmail.com>
2024-05-15 22:44:27 -0300

    Fix: CORS /auth

--- Oracle-Partner-Tracker/src/main/java/Oracle/Partner/Tracker/controllers/AuthenticationController.java
+++ Oracle-Partner-Tracker/src/main/java/Oracle/Partner/Tracker/controllers/AuthenticationController.java
@@ -1,6 +1,7 @@
 package Oracle.Partner.Tracker.controllers;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import Oracle.Partner.Tracker.services.AuthenticationService;
 
 
 @RestController
+@CrossOrigin
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
